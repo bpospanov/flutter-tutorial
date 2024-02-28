39cf2398de48841cfdae0a853c9b410088be62f4
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-02-28 15:35:34 +0600

    #27 - World Time API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -13,16 +13,25 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async {
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Almaty'));
     Map data = jsonDecode(response.body);
-    print(data['title']);
+    print(data);
+
+    String datetime = data['utc_datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    // print(datetime);
+    // print(offset);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
