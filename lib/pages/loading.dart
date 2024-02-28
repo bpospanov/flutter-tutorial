95db35b5f5667e25940daa5376d4135e1694fcd9
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-02-28 16:09:22 +0600

    #30 - Passing Route Data

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -12,15 +12,15 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  String time = 'loading';
-
   void setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Almaty', flag: 'kz.png', url: 'Asia/Almaty');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
     });
+
   }
   
   @override
@@ -34,7 +34,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(50.0),
-        child: Text(time),
+        child: Text('loading'),
       ),
     );
   }
