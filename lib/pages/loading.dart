4010f94285abadb1892c400197d2b2c880b1d85a
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2024-02-28 16:41:53 +0600

    #32 - Loaders / Spinners

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -32,10 +33,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Text('loading'),
-      ),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 69.0,
+        ),
+      )
     );
   }
 }
